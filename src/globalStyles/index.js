51af52fba2959e2904241ed3e34b07292930b60c
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-05-13 05:13:47 -0300

    chore: creating folders

---
+++ src/globalStyles/index.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+        font-family: 'Raleway', sans-serif;
+        
+    }
+    button {
+        cursor: pointer;
+        background: #fff;
+        border: none;
+    }
+    input{
+        background: #fff;
+        border: none;
+    }
+    a {
+        text-decoration: none;
+    }
+    body {
+        background: linear-gradient(180deg, rgba(225, 201, 255, 0.7) 0%, rgba(255, 255, 255, 0) 100%), #FFFFFF;
+    }
+    main{
+        overflow: auto;
+    }
+    h1{
+        cursor: default;
+    }
+    p{
+        cursor: default;
+    }
+`
