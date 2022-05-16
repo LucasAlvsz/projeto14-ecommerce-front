2e24ed463f0a8afd429cd18316de5322c7355495
Weslen Mendes <weslenmends@gmail.com>
2022-05-16 17:21:01 -0300

    fix: error in routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,13 +4,21 @@ import Home from "../pages/Home"
 import SignUp from "../pages/SignUp"
 import SignIn from "../pages/SignIn"
 import Checkout from "../pages/Checkout"
+import Loader from "../pages/Loader"
+import Home from "../pages/Home"
+import ProductId from "../pages/ProductId"
+import Cart from "../pages/Cart"
+import Search from "../pages/Search"
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/" element={<Loader />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/product/:id" element={<ProductId />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/search/:keyword" element={<Search />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/checkout" element={<Checkout />} />
