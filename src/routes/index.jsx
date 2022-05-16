83d8204be2ec48b2b8fcdd172a7aa87e621d2fa9
Weslen Mendes <weslenmends@gmail.com>
2022-05-16 17:26:53 -0300

    chore: routes problems

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,3 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Loader from "../pages/Loader"
+import Home from "../pages/Home"
+import ProductId from "../pages/ProductId"
+import Cart from "../pages/Cart"
+import Search from "../pages/Search"
+import SignUp from "../pages/SignUp"
+import SignIn from "../pages/SignIn"
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Loader />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/product/:id" element={<ProductId />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/search/:keyword" element={<Search />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
 // import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 // import Home from "../pages/Home"
