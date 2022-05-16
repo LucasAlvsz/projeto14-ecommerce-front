00f90cd047c574bb065d31f1c73b5f065c83be2c
Weslen Mendes <weslenmends@gmail.com>
2022-05-16 17:25:09 -0300

    chore: commented routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,29 +1,21 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
-import Home from "../pages/Home"
-import SignUp from "../pages/SignUp"
-import SignIn from "../pages/SignIn"
-import Checkout from "../pages/Checkout"
-import Loader from "../pages/Loader"
-import Home from "../pages/Home"
-import ProductId from "../pages/ProductId"
-import Cart from "../pages/Cart"
-import Search from "../pages/Search"
+// import Home from "../pages/Home"
+// import SignUp from "../pages/SignUp"
+// import SignIn from "../pages/SignIn"
+// import Checkout from "../pages/Checkout"
 
-export default function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Loader />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/product/:id" element={<ProductId />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/search/:keyword" element={<Search />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/checkout/:id" element={<Checkout />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+// export default function Router() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Navigate to="/home" />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/sign-up" element={<SignUp />} />
+//         <Route path="/sign-in" element={<SignIn />} />
+//         <Route path="/checkout" element={<Checkout />} />
+//         <Route path="/checkout/:id" element={<Checkout />} />
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
