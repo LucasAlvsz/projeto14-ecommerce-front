dcb5370390c6ec4067a11f58ebd416a1cfb76990
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-05-16 18:23:05 -0300

    chore: deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,17 @@
 import axios from "axios"
 
-const URL_BASE = "http://localhost:5000"
-
-export const SignUp = async (data) => {
-  try {
-    return await axios.post(`${URL_BASE}/sign-up`, data)
-  } catch (e) {
-    alert(e.response?.data.split(", ")[1])
-  }
+export const SignUp = async data => {
+	try {
+		return await axios.post(`${process.env.REACT_APP_URI}/sign-up`, data)
+	} catch (e) {
+		alert(e.response?.data.split(", ")[1])
+	}
 }
 
-export const SignIn = async (data) => {
-  try {
-    return await axios.post(`${URL_BASE}/sign-in`, data)
-  } catch (e) {
-    alert(e.response?.data.split(", ")[1])
-  }
+export const SignIn = async data => {
+	try {
+		return await axios.post(`${process.env.REACT_APP_URI}/sign-in`, data)
+	} catch (e) {
+		alert(e.response?.data.split(", ")[1])
+	}
 }
